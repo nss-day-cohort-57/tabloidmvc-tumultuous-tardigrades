37b1d1f4d4a216425c56746aefb222bb9384e738
Sutherland Seals <sseals1@gmail.com>
2022-09-29 14:29:47 -0500

    Added functionality to restrict the UserProfiles to only Users of type Admin

--- TabloidMVC/Controllers/AccountController.cs
+++ TabloidMVC/Controllers/AccountController.cs
@@ -40,6 +40,7 @@ namespace TabloidMVC.Controllers
             {
                 new Claim(ClaimTypes.NameIdentifier, userProfile.Id.ToString()),
                 new Claim(ClaimTypes.Email, userProfile.Email),
+                new Claim(ClaimTypes.Role, userProfile.UserType.Name)
             };
 
             var claimsIdentity = new ClaimsIdentity(
